13112c29e1fa0502fdd89ef0d3258d558570ecf7
Vaibhav0-1 <130361641+Vaibhav0-1@users.noreply.github.com>
2024-07-13 19:46:48 +0530

    initializing endpoint

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,19 @@ import { useMemo } from "react"
 import { BlogProvider } from "src/context/Blog"
 import { Router } from "src/router"
 import "./App.css"
-
+import {ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
 
 export const App = () => {
+  const endpoint = "https://indulgent-autumn-wave.solana-devnet.quiknode.pro/9e69048489ffe3a16ef9db9f008cb58c407b6b61/"
 
   return (
-    <BlogProvider>
-      <Router />
-    </BlogProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider>
+        <BlogProvider>
+          <Router />
+        </BlogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }

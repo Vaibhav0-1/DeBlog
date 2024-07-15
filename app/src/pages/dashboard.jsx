58919acdb85d7ffacd2b2dab338c947b8b63084d
Vaibhav0-1 <130361641+Vaibhav0-1@users.noreply.github.com>
2024-07-15 19:05:47 +0530

    added autoconnecting feature for wallet

--- app/src/pages/dashboard.jsx
+++ app/src/pages/dashboard.jsx
@@ -11,7 +11,7 @@ import { useHistory } from 'react-router-dom'
 export const Dashboard = () => {
   const history = useHistory()
   const [connecting, setConnecting] = useState(false)
-  const { select } = useWallet()
+  const { connected, select } = useWallet()
   const [postTitle, setPostTitle] = useState("")
   const [postContent, setPostContent] = useState("")
 
@@ -20,7 +20,7 @@ export const Dashboard = () => {
     name: "Random Robot",
     avatar: "https://avatarfiles.alphacoders.com/283/thumb-283778.jpg",
   }
-  const connected = true
+  //const connected = true
   const posts = []
 
   const createPost = () => {
@@ -31,7 +31,7 @@ export const Dashboard = () => {
   const setShowModal = () => {
 
   }
-  /////////////////
+  
 
   const onConnect = () => {
     setConnecting(true)
